9c28b69af847b0e33074fb3ce55b45818f97fbc1
Jzgolden <109767136+Jzgolden@users.noreply.github.com>
2023-06-27 17:50:32 +0000

    age.py
    demo if, elif, else statements

---
+++ age.py
@@ -0,0 +1,15 @@
+age = 4
+if age > 17:
+  print("adult")
+elif (age > 12):
+  print("teen")
+else:
+  print("child")
+
+age1 = 9
+if age1 < 13:
+  print("child")
+elif age1 < 18:
+  print("teen")
+else:
+  print("adult")
